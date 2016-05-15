70cff4a9bdc4d16c977bf7af8f1c82a0b409871b
Victor Alvarado <dev.honduras@gmail.com>
2016-05-14 23:21:40 -0600

    corrigiendo el multiple registro

--- config.php
+++ config.php
@@ -4,10 +4,10 @@
 // Important ! These must be filled in correctly.
 // Database details are required to use this script.
 
-$HOST = "localhost"; // If you don't know what your host is, it's safe to leave it localhost
-$USERNAME = "root"; // Database name
-$PASSWORD = ""; // Username
-$DB = "spapergcm"; // Password
+$HOST = "mysql.hostinger.es"; // If you don't know what your host is, it's safe to leave it localhost
+$USERNAME = "u160055481_vic89"; // Database name
+$PASSWORD = "vic891606"; // Username
+$DB = "u160055481_paper"; // Password
 
 define("GOOGLE_API_KEY", "AIzaSyCXBsGnoJG53gjoSyDpW2WQBws5L1NDxF4"); // Google API key
 
